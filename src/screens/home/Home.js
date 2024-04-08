3cb1af3faf5be21ba6927dcce79beb2b6d66c55b
Mahmoud_alqiaty <72821068+mahmoud-alqiaty@users.noreply.github.com>
2024-04-08 04:20:46 +0200

    Update Home.js try fix core in github

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -89,8 +89,13 @@ const Home = () => {
     //get data from db
     useEffect(() => {
         const getallData = async () =>{
-            axios.get("https://web-production-9b2e.up.railway.app/mapsAndSats/maps/635259f5f3b78e569fbbeb62")
-            // axios.get("http://212.103.189.111:8090/getdata")
+            // axios.get("https://web-production-9b2e.up.railway.app/mapsAndSats/maps/635259f5f3b78e569fbbeb62")
+            axios.get("http://212.103.189.111:8090/getdata", 
+                      headers: {
+                      Accept: 'application/json',
+                      'Access-Control-Allow-Origin': '*',
+                       },
+                     )
             .then(res=>{
               console.log("res from API1: ", res.data);
               console.log("fourDates: ", fourDates);
